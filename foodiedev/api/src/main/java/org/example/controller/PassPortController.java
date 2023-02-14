6c6c3c8de356e64d8f7780f347367e91caae63a2
zicheng <1094275633@qq.com>
2023-02-14 22:46:25 +0800

    设置cookie，并且浏览器可以获取cookie展示

--- foodiedev/api/src/main/java/org/example/controller/PassPortController.java
+++ foodiedev/api/src/main/java/org/example/controller/PassPortController.java
@@ -7,10 +7,15 @@ import org.apache.commons.lang3.StringUtils;
 import org.example.pojo.Users;
 import org.example.pojo.bo.UserBO;
 import org.example.service.UserService;
+import org.example.utils.CookieUtils;
 import org.example.utils.JSONResult;
+import org.example.utils.JsonUtils;
 import org.example.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Api(value = "注册登录", tags = {"用于注册登录的相关接口"})
 @RestController
@@ -42,7 +47,8 @@ public class PassPortController {
 
     @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("/regist")
-    public JSONResult regist(@RequestBody UserBO userBO) {
+    public JSONResult regist(@RequestBody UserBO userBO, HttpServletRequest request,
+                             HttpServletResponse response) {
 
         String userName = userBO.getUsername();
         String password = userBO.getPassword();
@@ -67,7 +73,9 @@ public class PassPortController {
             return JSONResult.errorMsg("两次密码输入不一致");
 
         // 4. 实现注册
-        userService.createUser(userBO);
+        Users userResult = userService.createUser(userBO);
+
+        CookieUtils.setCookie(request, response, "user", JsonUtils.objectToJson(userResult), true);
 
         return JSONResult.ok();
     }
@@ -75,7 +83,8 @@ public class PassPortController {
 
     @ApiOperation(value = "用户登录", notes = "用户登录", httpMethod = "POST")
     @PostMapping("/login")
-    public JSONResult login(@RequestBody UserBO userBO) throws Exception {
+    public JSONResult login(@RequestBody UserBO userBO, HttpServletRequest request,
+                            HttpServletResponse response) throws Exception {
 
         String userName = userBO.getUsername();
         String password = userBO.getPassword();
@@ -87,8 +96,26 @@ public class PassPortController {
         if (userResult == null) {
             return JSONResult.errorMsg("用户名或密码错误");
         }
+        userResult = setNullProperty(userResult);
+
+        // 向浏览器设置cookie, true表示加密
+        /*
+        '%7B%22id%22%3A%22230214G8WMPPR680%22%2C%22username%22%3A%22xiaoli%22%2C%22password%22%3Anull%2C%22nickname%22%3A%22xiaoli%22%2C%22realname%22%3Anull%2C%22face%22%3A%22https%3A%2F%2Fimg1.baidu.com%2Fit%2Fu%3D3486651663%2C3991438881%26fm%3D253%26app%3D138%26size%3Dw931%26n%3D0%26f%3DJPEG%26fmt%3Dauto%3Fsec%3D1675443600%26t%3D881bbc6a6cff601b189d3a0a2f8fde1b%22%2C%22mobile%22%3Anull%2C%22email%22%3Anull%2C%22sex%22%3A2%2C%22birthday%22%3Anull%2C%22createdTime%22%3Anull%2C%22updatedTime%22%3Anull%7D'
+         */
+        // 页面通过 decodeURIComponent(app.getCookie("user")) 解析加密cookie
+        CookieUtils.setCookie(request, response, "user", JsonUtils.objectToJson(userResult), true);
 
         return JSONResult.ok(userResult);
+    }
+
+    private Users setNullProperty(Users userResult) {
+        userResult.setBirthday(null);
+        userResult.setEmail(null);
+        userResult.setMobile(null);
+        userResult.setCreatedTime(null);
+        userResult.setUpdatedTime(null);
+        userResult.setPassword(null);
+        return userResult;
     }
 
 }
